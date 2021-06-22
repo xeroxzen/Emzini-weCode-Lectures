d52f842894d2fda99c698414582fc6d61e1467a3
Andile Jaden Mbele <andilembele020@gmail.com>
2021-06-23 00:11:07 +0200

    count_partitions

--- ex1.py
+++ ex1.py
@@ -309,5 +309,22 @@ def f_then_g(f, g, n):
         g(n)
 
 
-def grow(n): return f_then_g(grow, print, n // 10)
-def shrink(n): return f_then_g(print, shrink, n // 10)
+def grow(n):
+    return f_then_g(grow, print, n // 10)
+
+
+def shrink(n):
+    return f_then_g(print, shrink, n // 10)
+
+
+def count_partitions(n, m):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        with_m = count_partitions(n - m, m)
+        without_m = count_partitions(n, m - 1)
+        return with_m + without_m
